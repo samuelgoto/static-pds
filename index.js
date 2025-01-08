85f7d8026c625bae81e518434ec173b6d2210d68
Sam Goto <samuelgoto@gmail.com>
2025-01-07 17:12:14 -0800

    Allow handles from configurable domains

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import { TestPds } from "@atproto/dev-env";
 
 const port = process.env.PORT || 8080;
 const hostname = process.env.HOSTNAME || `localhost`;
+const domains = process.env.DOMAINS || ".test";
 
 const pds = await TestPds.create({
   port: port,
   hostname: hostname,
+  serviceHandleDomains: [domains],
 });
 
 console.log(`Running on ${hostname}.`);
