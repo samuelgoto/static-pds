f54ad09b3ac7e67f38a1ad038388d2c85db6d155
Sam Goto <samuelgoto@gmail.com>
2026-02-12 12:22:31 -0800

    Use @vercel/blob for the blob store

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-const { TestPds } = require("@atproto/dev-env");
+"use strict";
+const {
+  PDS,
+  envToCfg,
+  envToSecrets,
+  readEnv,
+  httpLogger,
+} = require("@atproto/pds");
+const pkg = require("@atproto/pds/package.json");
 
-// https://char.lt/blog/2024/10/atproto-pds/                                                                                                                              
+// https://char.lt/blog/2024/10/atproto-pds/
 
-const port = process.env.PORT || 8080;
-const hostname = process.env.HOSTNAME || `localhost`;
-const domains = process.env.DOMAINS || ".test";
-
-console.log(`$HOSTNAME=${process.env.HOSTNAME}`);
-console.log(`$DOMAINS=${process.env.DOMAINS}`);
-
-async function main() {
-
-  const pds = await TestPds.create({
-    devMode: false,
-    port: port,
-    hostname: hostname,
-    serviceHandleDomains: [domains],
-    bskyAppViewUrl: "https://api.pop1.bsky.app",
-    bskyAppViewDid: "did:web:api.bsky.app",
+const main = async () => {
+  const env = readEnv();
+  env.version ||= pkg.version;
+  const cfg = envToCfg(env);
+  const secrets = envToSecrets(env);
+  const pds = await PDS.create(cfg, secrets);
+  await pds.start();
+  httpLogger.info("pds has started");
+  pds.app.get("/tls-check", (req, res) => {
+    checkHandleRoute(pds, req, res);
   });
+  // Graceful shutdown (see also https://aws.amazon.com/blogs/containers/graceful-shutdowns-with-ecs/)
+  process.on("SIGTERM", async () => {
+    httpLogger.info("pds is stopping");
+    await pds.destroy();
+    httpLogger.info("pds is stopped");
+  });
+};
 
-  console.log(`Running on ${hostname}.`);
+async function checkHandleRoute(
+  /** @type {PDS} */ pds,
+  /** @type {import('express').Request} */ req,
+  /** @type {import('express').Response} */ res
+) {
+  try {
+    const { domain } = req.query;
+    if (!domain || typeof domain !== "string") {
+      return res.status(400).json({
+        error: "InvalidRequest",
+        message: "bad or missing domain query param",
+      });
+    }
+    if (domain === pds.ctx.cfg.service.hostname) {
+      return res.json({ success: true });
+    }
+    const isHostedHandle = pds.ctx.cfg.identity.serviceHandleDomains.find(
+      (avail) => domain.endsWith(avail)
+    );
+    if (!isHostedHandle) {
+      return res.status(400).json({
+        error: "InvalidRequest",
+        message: "handles are not provided on this domain",
+      });
+    }
+    const account = await pds.ctx.accountManager.getAccount(domain);
+    if (!account) {
+      return res.status(404).json({
+        error: "NotFound",
+        message: "handle not found for this domain",
+      });
+    }
+    return res.json({ success: true });
+  } catch (err) {
+    httpLogger.error({ err }, "check handle failed");
+    return res.status(500).json({
+      error: "InternalServerError",
+      message: "Internal Server Error",
+    });
+  }
 }
 
 main();
